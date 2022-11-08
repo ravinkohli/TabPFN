3c1b710d764c5134ba14e6f7bfb8d5f2187386c1
Ravin Kohli <kohliravin7@gmail.com>
2022-11-08 15:59:18 +0100

    working svm with seeds

--- eval_utils.py
+++ eval_utils.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
+import argparse
 import pickle
+import re
 from dataclasses import dataclass
 from functools import partial
+from itertools import product
 from pathlib import Path
 from typing import Callable
 
 import numpy as np
-
+import pandas as pd
 import random
-
 import torch
 
 import tabpfn.scripts.tabular_baselines as tb
@@ -18,7 +20,31 @@ from tabpfn.scripts.tabular_baselines import clf_dict
 from tabpfn.scripts.tabular_evaluation import evaluate
 from tabpfn.scripts.tabular_metrics import (accuracy_metric, auc_metric,
                                             brier_score_metric,
-                                            cross_entropy, ece_metric)
+                                            calculate_score, cross_entropy,
+                                            ece_metric, time_metric)
+
+HERE = Path(__file__).parent.resolve().absolute()
+
+METRICS = {
+    "roc": auc_metric,
+    "cross_entropy": cross_entropy,
+    "acc": accuracy_metric,
+    "brier_score": brier_score_metric,
+    "ece": ece_metric,
+}
+
+PREDEFINED_RESULTS_PATH = HERE / "TabPFNResults" / "all_results"
+PREDFINED_DATASET_PATHS = HERE / "tabpfn" / "datasets"
+PREDEFINED_DATASET_COLLECTIONS = {
+    "cc_valid": {
+        "ids": open_cc_valid_dids,
+        "path": PREDFINED_DATASET_PATHS / "cc_valid_datasets_multiclass.pickle",
+    },
+    "cc_test": {
+        "ids": open_cc_dids,
+        "path": PREDFINED_DATASET_PATHS / "cc_test_datasets_multiclass.pickle",
+    },
+}
 
 DEFAULT_SEED = 42
 
@@ -68,7 +94,9 @@ class Dataset:
 
     @classmethod
     def fetch(
-        self, identifier: str | int | list[int], only: Callable | None = None
+        self,
+        identifier: str | int | list[int],
+        only: Callable | None = None,
     ) -> list[Dataset]:
         if isinstance(identifier, str) and identifier in PREDEFINED_DATASET_COLLECTIONS:
             datasets = Dataset.from_predefined(identifier)
@@ -134,7 +162,7 @@ class Dataset:
         ]
 
     def as_list(self) -> list:
-        """How the internals expect a dataset to look like"""
+        """How the internals expect a dataset to look like."""
         return [
             self.name,
             self.X,
@@ -143,6 +171,129 @@ class Dataset:
             self.attribute_names,
             self.info,
         ]
+
+
+@dataclass
+class Row:
+    time: int
+    pos: int
+    method: str
+    split: int
+    metric: str
+    metric_value: dict[str, float]
+
+
+@dataclass
+class Results:
+    # Big ass predefined dictionary
+    df: pd.DataFrame
+
+    @classmethod
+    def from_dict(
+        self,
+        d: dict,
+        datasets: list[Dataset],
+        recorded_metrics: list[str],
+        *,
+        dropna: bool = True
+    ) -> Results:
+        # TODO: we could extract dataset_names for the dict but it's not ordered well
+        #   for that. Likewise for the recorded_metrics
+        #
+        # We do a lot of parsing here to massage things into a nice table
+        # Extract all the times listed in the keys
+        pattern = re.compile(
+            r"(?P<method>\w+)"
+            r"_time_(?P<time>\d+(\.\d+)?)"
+            r"(_)?(?P<metric>\w+)"
+            r"_split_(?P<split>\d+)"
+        )
+
+        groups = []
+        for key in d:
+            match = re.match(pattern, key)
+            if not match:
+                raise ValueError(key)
+
+            groups.append(match.groupdict())
+
+        matches = pd.DataFrame(groups)
+
+        # The unique, methods, times, metrics and splits present
+        methods = list(matches["method"].unique())
+        times = list(matches["time"].astype(float).unique())
+        metrics = list(matches["metric"].unique())
+        splits = list(matches["split"].astype(int).unique())
+
+        # Next we extract all the eval_positions
+        _eval_positions = set()
+        for v in d.values():
+            _eval_positions.update(v["eval_positions"])
+        eval_positions = sorted(_eval_positions)
+
+        # Dataset names...
+        dataset_names = sorted([d.name for d in datasets])
+
+        # We flatten out the fit_time and inference_time of best_config
+        for (k, v), pos, dataset in product(d.items(), eval_positions, datasets):
+            old_best_configs_key = f"{dataset.name}_best_configs_at_{pos}"
+
+            best_config_key = f"{dataset.name}_best_config"
+            inference_time_key = f"{dataset.name}_inference_time_at_{pos}"
+            fit_time_key = f"{dataset.name}_fit_time_at_{pos}"
+
+            # If there is a best config
+            if any(v.get(old_best_configs_key, [])):
+                assert len(v[old_best_configs_key]) == 1
+
+                best_config = v[old_best_configs_key][0]
+
+                v[inference_time_key] = best_config.get("inference_time", np.nan)
+                v[fit_time_key] = best_config.get("fit_time", np.nan)
+                v[best_config_key] = best_config.copy()
+                del v[old_best_configs_key]
+            else:
+                v[inference_time_key] = np.nan
+                v[fit_time_key] = np.nan
+                v[best_config_key] = np.nan
+
+        index = pd.MultiIndex.from_product(
+            [methods, times, splits, metrics, eval_positions],
+            names=["method", "time", "split", "optimization_metric", "eval_position"],
+        )
+
+        metrics = recorded_metrics + ["time", "inference_time", "fit_time"]
+        columns = pd.MultiIndex.from_product(
+            [dataset_names, metrics],
+            names=["dataset", "metric"],
+        )
+
+        df = pd.DataFrame(columns=columns, index=index)
+        df.sort_index(inplace=True)
+
+        for k, v in d.items():
+            match = re.match(pattern, k)
+            if match is None:
+                raise ValueError(k)
+
+            method = match.group("method")
+            time = float(match.group("time"))
+            opt_metric = match.group("metric")
+            split = int(match.group("split"))
+
+            for dataset, metric, pos in product(dataset_names, metrics, eval_positions):
+                row = (method, time, split, opt_metric, int(pos))
+                col = (dataset, metric)
+
+                value = v.get(f"{dataset}_{metric}_at_{pos}", np.nan)
+
+                df.loc[row, col] = value
+
+        # Drop full NaN rows
+        if dropna:
+            df = df[df.any(axis=1)]
+
+        return Results(df)
 
 
 # Predefined methods with `no_tune={}` inidicating they are not tuned
@@ -204,15 +355,15 @@ def eval_method(
     datasets: list[Dataset],
     label: str,
     classifier_evaluator: Callable,
-    max_time: int | None,
+    max_time: float | None,
     metric_used: Callable,
-    split_id: int,
+    split: int,
+    eval_positions: list[int],
     result_path: Path,
-    seed: int = DEFAULT_SEED,
     append_metric: bool = True,
     fetch_only: bool = False,
     verbose: bool = False,
-    eval_positions: list[int] = [1000],
+    seed: int = DEFAULT_SEED,
     bptt: int = 2000,
     overwrite: bool = False,
 ):
@@ -240,14 +391,163 @@ def eval_method(
         base_path=result_path,
         eval_positions=eval_positions,
         device=device,
+        max_splits=1,
         overwrite=overwrite,
         save=True,
         metric_used=metric_used,
         path_interfix=task_type,
         fetch_only=fetch_only,
-        split_number=split_id,
+        split_id=split,
         verbose=verbose,
         max_time=max_time,
-        seed=seed,
-        split_id=split_id
+        seed=seed
+    )
+
+
+def arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--result_path",
+        type=Path,
+        help="Where the results path is",
+        default=HERE,
+    )
+    parser.add_argument("--gpu", action="store_true", help="GPU's available?")
+    parser.add_argument(
+        "--times",
+        nargs="+",
+        type=float,
+        default=[30],
+        help="Times to evaluate (seconds)",
+    )
+    parser.add_argument(
+        "--seeds",
+        nargs="+",
+        type=float,
+        default=[DEFAULT_SEED],
+        help="Seeds to evaluate",
+    )
+    parser.add_argument(
+        "--splits",
+        type=int,
+        default=5,
+        help="The splits to evaluate",
+    )
+    parser.add_argument(
+        "--validation_datasets",
+        nargs="+",
+        type=int,
+        help="The validation datasets",
+    )
+    parser.add_argument(
+        "--test_datasets",
+        nargs="+",
+        type=int,
+        help="The test datasets",
+    )
+    parser.add_argument(
+        "--optimization_metrics",
+        type=str,
+        choices=METRICS,
+        help="Metrics to optimize for (if possible)",
+        default=["roc"],
+    )
+    parser.add_argument(
+        "--recorded_metrics",
+        type=str,
+        nargs="+",
+        choices=METRICS,
+        help="Metrics to calculate for results",
+        default=["roc", "cross_entropy", "acc", "brier_score", "ece"],
+    )
+    parser.add_argument(
+        "--methods",
+        choices=METHODS.keys(),
+        nargs="+",
+        type=str,
+        help="The methods to evaluate",
+        default=["svm_default"],
+    )
+    parser.add_argument(
+        "--fetch_only",
+        action="store_true",
+        help="Whether to only fetch results and not run anything",
+    )
+
+    # Transformer args
+    parser.add_argument(
+        "--bptt",
+        type=int,
+        help="Transformer sequence length",
+        default=2000,
+    )
+    parser.add_argument("--eval_positions", nargs="+", type=int, default=[1_000])
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        help="Whether to overwrite results if they already exist",
+    )
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--plots", type=Path, help="Where to output plots to")
+    parser.add_argument("--load_predefined_results", action="store_true")
+    parser.add_argument(
+        "--predefined_results_path", type=Path, default=PREDEFINED_RESULTS_PATH
+    )
+    return parser.parse_args()
+
+
+def do_evaluations(args: argparse.Namespace, datasets: list[Dataset]) -> Results:
+    results = {}
+    for seed, method, metric, time, split in product(
+        args.seeds,
+        args.methods,
+        args.optimization_metrics,
+        args.times,
+        range(0, args.splits),
+    ):
+        metric_f = METRICS[metric]
+        metric_name = tb.get_scoring_string(metric_f, usage="")
+        key = f"{method}_time_{time}{metric_name}_split_{split}_seed_{seed}"
+
+        results[key] = eval_method(
+            datasets=datasets,
+            label=method,
+            result_path=args.result_path,
+            classifier_evaluator=METHODS[method],
+            eval_positions=args.eval_positions,  # It's a constant basically
+            fetch_only=args.fetch_only,
+            verbose=args.verbose,
+            max_time=time,
+            metric_used=metric_f,
+            split=split,
+            seed=seed,
+            overwrite=args.overwrite,
+        )
+
+    datasets_as_lists = [d.as_list() for d in datasets]
+
+    # This will update the results in place
+    for metric in args.recorded_metrics:
+        metric_f = METRICS[metric]
+        calculate_score(
+            metric=metric_f,
+            name=metric,
+            global_results=results,
+            ds=datasets_as_lists,
+            eval_positions=args.eval_positions,
+        )
+
+    # We also get the times
+    calculate_score(
+        metric=time_metric,
+        name="time",
+        global_results=results,
+        ds=datasets_as_lists,
+        eval_positions=args.eval_positions,
+    )
+
+    return Results.from_dict(
+        results,
+        datasets=datasets,
+        recorded_metrics=args.recorded_metrics + ["time"],
     )
--- evaluation.py
+++ evaluation.py
@@ -5,105 +5,14 @@ from pathlib import Path
 
 import numpy as np
 
+import pickle
 
-import tabpfn.scripts.tabular_baselines as tb
-from tabpfn.scripts.tabular_metrics import (calculate_score, count_metric,
-                                            time_metric)
 
-from eval_utils import Dataset, DEFAULT_SEED, HERE, METHODS, METRICS, eval_method, set_seed
+from eval_utils import Dataset, Results, arguments, do_evaluations, DEFAULT_SEED, HERE, METHODS, METRICS, eval_method, set_seed
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--result_path",
-        type=Path,
-        help="Where the results path is",
-        default=HERE,
-    )
-    parser.add_argument("--gpu", action="store_true", help="GPU's available?")
-    parser.add_argument(
-        "--times",
-        nargs="+",
-        type=int,
-        default=[30],
-        help="Times to evaluate (seconds)",
-    )
-    parser.add_argument(
-        "--seeds",
-        nargs="+",
-        type=int,
-        default=[DEFAULT_SEED],
-        help="Seeds to evaluate on",
-    )
-    parser.add_argument(
-        "--splits",
-        type=int,
-        default=5,
-        help="The number of splits to evaluate",
-    )
-
-    parser.add_argument(
-        "--validation_datasets",
-        nargs="+",
-        type=int,
-        help="The validation datasets",
-    )
-    parser.add_argument(
-        "--test_datasets",
-        nargs="+",
-        type=int,
-        help="The test datasets",
-    )
-
-    parser.add_argument(
-        "--optimization_metrics",
-        type=str,
-        choices=METRICS,
-        help="Metrics to optimize for (if possible)",
-        default=["roc"],
-    )
-    parser.add_argument(
-        "--result_metrics",
-        type=str,
-        nargs="+",
-        choices=METRICS,
-        help="Metrics to calculate for results",
-        default=["roc", "cross_entropy", "acc", "brier_score", "ece"],
-    )
-
-    parser.add_argument(
-        "--methods",
-        choices=METHODS.keys(),
-        nargs="+",
-        type=str,
-        help="The methods to evaluate",
-        default=["svm_default"],
-    )
-    parser.add_argument(
-        "--fetch_only",
-        action="store_true",
-        help="Whether to only fetch results and not run anything",
-    )
-
-    # Transformer args
-    parser.add_argument(
-        "--bptt",
-        type=int,
-        help="Transformer sequence length",
-        default=2000,
-    )
-    parser.add_argument(
-        "--overwrite",
-        action="store_true",
-        help="Whether to overwrite results if they already exist",
-    )
-    parser.add_argument("--verbose", action="store_true")
-    parser.add_argument("--plots", type=Path, help="Where to output plots to")  
-
-    args = parser.parse_args()
+    args = arguments()
 
     if not args.validation_datasets:
         args.validation_datasets = "cc_valid"
@@ -121,72 +30,27 @@ if __name__ == "__main__":
 
     valid_datasets = Dataset.fetch(args.validation_datasets, only=filter_f)
     test_datasets = Dataset.fetch(args.test_datasets, only=filter_f)
+
     all_datasets = valid_datasets + test_datasets
-    all_datasets = all_datasets[:2]
+    all_datasets = all_datasets
 
-    results = {}
-    for seed, method, metric, time in product(
-        args.seeds,
-        args.methods,
-        args.optimization_metrics,
-        args.times,
-    ):
-        metric_f = METRICS[metric]
-        metric_name = tb.get_scoring_string(metric_f, usage="")
+    if not args.load_predefined_results:
+        result = do_evaluations(args, all_datasets)
+    else:
 
-        key = f"{method}_time_{time}_{metric_name}"
+        def read(_path: Path) -> dict:
+            with _path.open("rb") as f:
+                return pickle.load(f)
 
-        set_seed(seed=seed)
-
-        for split_id in range(args.splits):
-            print(f"Running split_{split_id}")
-            key += f"_split_{split_id}"
-            results[key] = eval_method(
-                datasets=all_datasets,
-                label=method,
-                result_path=args.result_path,
-                classifier_evaluator=METHODS[method],
-                eval_positions=[1_000],  # It's a constant basically
-                fetch_only=args.fetch_only,
-                verbose=args.verbose,
-                max_time=time,
-                metric_used=metric_f,
-                split_id=split_id,
-                overwrite=args.overwrite,
-                seed=seed,
-                bptt=args.bptt,
-            )
-
-    all_datasets_as_lists = [d.as_list() for d in all_datasets]
-    # This will update the results in place
-    for metric in args.result_metrics:
-        metric_f = METRICS[metric]
-        calculate_score(
-            metric=metric_f,
-            name=metric,
-            global_results=results,
-            ds=all_datasets_as_lists,
-            eval_positions=[1_000] + [-1],
+        d = {
+            path.stem: read(path)
+            for path in args.predefined_results_path.iterdir()
+            if path.is_file()
+        }
+        result = Results.from_dict(
+            d,
+            datasets=all_datasets,
+            recorded_metrics=args.recorded_metrics,
         )
 
-    # We also do some other little bits
-    for agg in ["sum", "mean"]:
-        calculate_score(
-            metric=time_metric,
-            name="time",
-            global_results=results,
-            ds=all_datasets_as_lists,
-            eval_positions=[1_000] + [-1],
-            aggregator=agg,
-        )
-
-    calculate_score(
-        metric=count_metric,
-        name="count",
-        global_results=results,
-        ds=all_datasets_as_lists,
-        eval_positions=[1_000] + [-1],
-        aggregator="sum",
-    )
-
-    # print(results)
+    df = result.df
