2c7ed68f52604ac28ec3a2c037c072a830fec263
Ravin Kohli <kohliravin7@gmail.com>
2022-11-09 14:40:45 +0100

    fix typo in autoexecutor

--- eval_utils.py
+++ eval_utils.py
@@ -25,7 +25,7 @@ from tabpfn.scripts.tabular_metrics import (accuracy_metric, auc_metric,
                                             brier_score_metric,
                                             calculate_score, cross_entropy,
                                             ece_metric, time_metric)
-from submitit import SlurmExecutor, AutoExecuter
+from submitit import SlurmExecutor, AutoExecutor
 
 DEFAULT_SEED = 42
 
@@ -59,7 +59,7 @@ class BoschSlurmExecutor(SlurmExecutor):
 
 PARTITION_TO_EXECUTER = {
     'bosch': BoschSlurmExecutor,
-    'other': AutoExecuter
+    'other': AutoExecutor
 
 }
 
